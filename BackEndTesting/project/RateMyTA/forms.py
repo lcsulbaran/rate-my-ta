68fb2ef0550059ce348826c11c0524b72aa389f3
DESKTOP-FJAJPC0\Harry <77893070+harrisonMondragon@users.noreply.github.com>
2022-03-19 14:27:22 -0600

    front to back with front end pages

--- BackEndTesting/project/RateMyTA/forms.py
+++ BackEndTesting/project/RateMyTA/forms.py
@@ -2,9 +2,10 @@ import email
 from django import forms 
 
 class SignupForm(forms.Form): 
-    name = forms.CharField(label = 'Your Name', max_length = 100)
-    email = forms.CharField(label = 'Email', max_length = 100)
-    password = forms.CharField(label = 'Password', max_length = 100)
+    #name = forms.CharField(label = 'Your Name', max_length = 100)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'floatingInput'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'floatingInput'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'floatingPassword'}))
 
 class LoginForm(forms.Form):
     email = forms.CharField(label = 'Email', max_length = 100)
