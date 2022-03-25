ad35490316f799bfd728b0999d54b67ef9796869
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2022-03-25 15:32:13 -0600

    added review validation

--- BackEndTesting/project/RateMyTA/forms.py
+++ BackEndTesting/project/RateMyTA/forms.py
@@ -1,5 +1,6 @@
 import email
 from django import forms 
+from django.core.exceptions import ValidationError
 
 class SignupForm(forms.Form): 
     #name = forms.CharField(label = 'Your Name', max_length = 100)
@@ -18,5 +19,5 @@ class NewReviewForm(forms.Form):
     courseCode = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'floatingTextArea'}))
     title = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'floatingTextArea'}))
     body = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'floatingTextArea2', 'style': 'height: 100px'}))
-    rating = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'class': 'form-select', 'id': 'floatingSelect'}))
+    rating = forms.IntegerField(required=True, min_value = 0, max_value = 10, widget=forms.NumberInput(attrs={'class': 'form-select', 'id': 'floatingSelect'}))
     taID = forms.CharField(widget=forms.HiddenInput(), required=False)
